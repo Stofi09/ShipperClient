b13abd51c380f0906c8bf745c8958d90ca434799
Stofi09 <kristof.solak@gmail.com>
2023-12-29 13:26:37 +0100

    basic config added

--- shipper-app/src/app/app.routes.ts
+++ shipper-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from "./compoments/register/register.component";
 
-export const routes: Routes = [];
+export const AppRoutes: Routes = [
+    { path: 'register', component: RegisterComponent },
+    // ... other routes
+  ];
--- shipper-app/src/app/compoments/register/register.component.ts
+++ shipper-app/src/app/compoments/register/register.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
   standalone: true,
-  imports: [],
+  selector: 'app-register',
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+    console.log(this.registerForm.value);
+  }
 }
